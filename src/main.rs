789c0d709c6b1d4d1ee898bba7d4451bb0247e0d
Paula Lavalle <768965+paulathevalley@users.noreply.github.com>
2024-01-15 17:12:45 -0800

    Rename to main. Use cargo test.

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::fs; // Source: https://doc.rust-lang.org/stable/book/ch12-02-reading-a-file.html
+
+// Day 1
+// https://adventofcode.com/2023/day/1
+
+// Source: https://doc.rust-lang.org/stable/rust-by-example/std_misc/file/read_lines.html
+fn read_lines(filename: &str) -> Vec<String> {
+    fs::read_to_string(filename)
+        .unwrap() // panic on possible file-reading errors
+        .lines() // split the string into an iterator of string slices
+        .map(String::from) // make each slice into a string
+        .collect() // gather them together into a vector
+}
+
+// Source: https://stackoverflow.com/questions/21747136/how-do-i-print-in-rust-the-type-of-a-variable
+// Usage: print_type_of(&line);
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+fn main() {
+    let file_path = "./data/day1.txt";
+
+    let lines: Vec<String> = read_lines(file_path);
+
+    let mut calibration: Vec<i32> = vec![];
+    // let mut i = 0;
+
+    // // Consumes the iterator, returns an (Optional) String
+    for line in lines {
+        // line has type "alloc::string::String"
+        calibration.push(calibration_value(&line));
+    }
+
+    for c in calibration {
+        println!("calibration {c}");
+    }
+}
+
+fn calibration_value(line: &str) -> i32 {
+    let digits: Vec<&str> = line.matches(char::is_numeric).collect();
+
+    let first_digit = &digits[0];
+    let last_digit = &digits[digits.len() - 1];
+
+    let result: i32 = format!("{first_digit}{last_digit}").parse().unwrap();
+
+    return result;
+}
+
+#[test]
+fn part1_calibration_first_line() {
+    let input = "1abc2";
+    let output = calibration_value(&input);
+    assert_eq!(output, 12);
+}
+
+// before running a rust program, you must compile it first
+// `$ rustc main.rs`
+// it will generate a binary executable
+// `$ ls`
+// `main    main.rs`
+// run the file
+// `$ ./main`
